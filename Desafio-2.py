a20c33a03bf1fdcdcae501aa4566d1f98cb8adc5
Thais-Dias12 <thais.dias.n483@gmail.com>
2023-08-28 15:24:47 -0300

    Desafio-2

---
+++ Desafio-2.py
@@ -0,0 +1,4 @@
+dia = input("\033[32m Qual dia você nasceu? \033[32m")
+mes = input("\033[33m Qual mês você nasceu? \033[33m")
+ano = input("\033[35m Qual ano você nasceu? \033[35m")
+print("\033[34m Você nasceu no dia \033[34m \033[32m {} \033[32m \033[34m do mês \033[34m \033[33m {} \033[33m \033[34m do ano \033[34m \033[35m {} \033[35m".format(dia, mes, ano))
---
+++ Desafio-3.py
@@ -0,0 +1,4 @@
+n1 = int(input('\033[36m Digite um número: '))
+n2 = int(input('\033[34m Digite outro número: '))
+s = n1 + n2 
+print('A soma entre \033[35m {} \033[35m \033[34m e \033[36m {} \033[36m \033[34m é igual a \033[31m {}'.format(n1, n2, s))
