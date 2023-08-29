6224da71659568eeec048379f9c0b99d18bbaf7f
Thais-Dias12 <thais.dias.n483@gmail.com>
2023-08-29 17:36:53 -0300

    Atualização

--- Desafio-13.py
+++ Desafio-13.py
@@ -1,3 +1,12 @@
+#Calculando  aumento do salario do funcionario:
 salario = float(input('\033[35m Qual é o salário do funcionario? R$'))
-aumento = salario + (salario * 15 / 100)
-print('\033[35m O salário do funcionario \033[32mR${:.2f}\033[35m, com 15% de aumento, passa a receber \033[32mR${:.2f}'.format(salario, aumento))
+if salario <= 1200:
+ print('\033[35m O salário do funcionario \033[32mR${:.2f}\033[35m, com 15% de aumento, passa a receber \033[32mR${:.2f}'.format(salario, salario + (salario * 15 / 100)))
+elif salario <= 1300:
+ print('\033[35m O salário do funcionario \033[32mR${:.2f}\033[35m, com 25% de aumento, passa a receber \033[32mR${:.2f}'.format(salario, salario + (salario * 25 / 100)))
+elif salario <= 1400: 
+ print('\033[35m O salário do funcionario \033[32mR${:.2f}\033[35m, com 35% de aumento, passa a receber \033[32mR${:.2f}'.format(salario, salario + (salario * 35 / 100)))
+elif salario <= 1500:
+ print('\033[35m O salário do funcionario \033[32mR${:.2f}\033[35m, com 45% de aumento, passa a receber \033[32mR${:.2f}'.format(salario, salario + (salario * 45 / 100)))
+elif salario <= 1600:
+ print('\033[35m O salário do funcionario \033[32mR${:.2f}\033[35m, com 50% de aumento, passa a receber \033[32mR${:.2f}'.format(salario, salario + (salario * 50 / 100)))
